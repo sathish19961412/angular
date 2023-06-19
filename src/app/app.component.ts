99963322389131ee38c8b4fda228e4c2aed24509
sathish19961412 <sathishfreelanc5@gmail.com>
2023-06-19 13:41:28 +0530

    basic twoway binding and binding method practices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- name:string="sathish";
+ btnDisable:boolean=true;
+ name!:string;
  username:string="sathish@1412199";
  City:string="Coimbatore";
  age:number=26;
  age1:number=27;
- isdisabled:boolean=false;
+ isdisabled:boolean=true;
+ text:string="No name is added";
 
+
+ constructor(){
+   setTimeout(()=>{
+    this.btnDisable=false;
+   },3000)
+ }
  getCity(){
     return "Coimbatore"
  }
  changeName(){
-   this.username="sathish@1412";
+    this.username="sathish@1412";
  }
  change(event:Event){
     this.username=(<HTMLInputElement>event.target).value;
+ }
+ addName(){
+    this.text=" Your name is " + " " + this.name;
  }
 }
